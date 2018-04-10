7cb113a15168191a3bf47f0cf58474b70280bb86
Leo <479711970@qq.com>
2018-04-10 23:16:58 +0800

    封装一个jsonp

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const data = {'data':'hello'};
+const url = require('url');
+const queryString = require('querystring');
+
+http.createServer(function(req, res) {
+  var params = url.parse(req.url);
+  console.log(params);
+  if(params.query && queryString.parse(params.query).callback) {
+    console.log(1231232);
+    const str = queryString.parse(params.query).callback + '(' + JSON.stringify(data) + ')';
+    return res.end(str)
+  }
+  res.end(JSON.stringify(data));
+}).listen(5000);
